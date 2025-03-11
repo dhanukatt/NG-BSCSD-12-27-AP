ac73f36ca5e8f5cabe52b270ec66fb2b0cdebb3c
Dhanuka Dilshan <dhanukatt03@gmail.com>
2025-03-11 20:50:03 +0530

    Update main.tsx

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,18 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import React from 'react';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+import HomePage from './pages/HomePage';
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const App: React.FC = () => {
+  return (
+    <div className="bg-gray-900 text-gray-300 min-h-screen flex flex-col">
+      <Navbar />
+      <main className="flex-grow p-6">
+        <HomePage />
+      </main>
+      <Footer />
+    </div>
+  );
+};
+
+export default App;
